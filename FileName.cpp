77964cbcb1de0590b2e42a81f292d2a83a32e00c
ValeriaRabch <rabchenukv@gmail.com>
2024-02-11 13:03:21 +0200

    Add func NumberToString

--- FileName.cpp
+++ FileName.cpp
@@ -41,12 +41,52 @@ int StringToNumber(char* str) {                             //2
 	return number;
 }
 
+char* NumberToString(int number) {          //3	
+	int q = 1;
+	for (int i = 10;; i *= 10) {
+		if (number < i) {
+			break;
+		}
+		q++;
+	}
+	char str[100];	
+	int r = 0, nom = number / pow(10, q - 1), w, index;
+	q--; w = pow(10, q);
+
+	for (int i = 0; q != -1; i++) {
+		if (q == 0) {
+			w = number;
+		}		
+		for (char j = '0'; j <= '9'; j++) {
+			if (nom == r) {
+				str[i] = j;
+				index = i;				
+				r = 0;
+				break;
+			}
+			r++;
+		}
+		q--;		
+		if (q != -1) {
+			w = pow(10, q);
+		}
+		nom = (number / w) % 10;
+	}
+
+	str[index + 1] = '\0';	
+	char* p = str;
+	return p;
+}
+
 int main() {
 	char str1[20];
 	char str2[20];
-	cin >> str1;
-	cin >> str2;
-	cout << StringToNumber(str1);
+	int number;
+	cin >> number;
+	char * p = NumberToString(number);
+	for (int i = 0; p[i] != '\0'; i++) {
+		cout << p[i];
+	}
 
 	return 0;
 }
